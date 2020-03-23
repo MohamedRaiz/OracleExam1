a47dbda4c7424b997fa0e9e097031c7a8cf06e83
Raiz <mohamed_raiz@hotmail.com>
2020-03-23 23:15:50 +0800

    on LVTI

---
+++ src/main/java/LVTI/Test/VarDonts.java
@@ -0,0 +1,20 @@
+package LVTI.Test;
+
+public class VarDonts {
+//    //Invalid - static class variables cannot be declared with var
+//    static var classVariable = 10;
+//
+//    //Invalid - class instance variable cannot be declared with var
+//    var instanceVariable = 20;
+//
+//    public static void main(String[] args) {
+//    }
+//
+//    // cannot have a return type of var for methods
+//    public static var returnThis(String[] args) {
+//
+//    }
+//
+//    //cannot pass in var as an parameters.
+//    public static String[] returnThat(var args) {}
+}
---
+++ src/main/java/LVTI/Test/VarTest.java
@@ -0,0 +1,31 @@
+package LVTI.Test;
+
+/*
+Learn Programming Academy's Java 1Z0-815 Certification Exam Course
+Section 3: Working with Java Primitive Data Types and String APIs
+Topic: Local Variable Type Inference
+*/
+
+import LVTI.com.WordyCompanyName.ALongPackageName.AClassWithAVeryLongName;
+
+public class VarTest {
+    public static void main(String[] args) {
+        var aClassWithAVeryLongName = new AClassWithAVeryLongName();
+        aClassWithAVeryLongName.setName("Testing");
+        System.out.println(aClassWithAVeryLongName);
+
+        // Adding some other var declarations:
+        // i is inferred to be an int, since it's assigned a literal int
+        var i = 1;
+
+        // An array can be assigned to an LVTI variable
+        var aVarArray = new int[3];
+
+        // Valid to assign a method return value to an LVTI variable
+        var methodVal = aClassWithAVeryLongName.getName();
+
+        // OK to assign a null object to LVTI variable but not literal null
+        Object nullObject = null;
+        var var = nullObject;
+    }
+}
