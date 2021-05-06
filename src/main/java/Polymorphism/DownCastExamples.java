e8b08b133daa7cbccfbdc8b663ee5d461ced3621
Raiz <mohamed_raiz@hotmail.com>
2021-05-06 21:59:27 +0800

    More updates

---
+++ src/main/java/Polymorphism/DownCastExamples.java
@@ -0,0 +1,68 @@
+package Polymorphism;
+
+
+class Animal {
+    public void printAnimal() {
+        System.out.println("I am an animal");
+    }
+}
+
+class Dog extends Animal {
+    public void printDog() {
+        System.out.println("I am A Dog");
+    }
+}
+
+class Cat extends Animal {
+    public void printCat() {
+        System.out.println("I am a Cat");
+    }
+}
+
+public class DownCastExamples {
+    public static void main(String[] args) {
+        DownCastExamples dex = new DownCastExamples();
+        //We create an instance of dog and cat but assign them to variables
+        // of type Animal
+        Animal genericDog = new Dog();
+        Animal genericCat = new Cat();
+
+        //We cast them now
+        dex.testDog((Dog) genericDog);
+        dex.testCat((Cat) genericCat);
+
+        //Try overloaded methods with specifically typed variables.
+        dex.testAnimal((Dog) genericDog);
+        dex.testAnimal((Cat) genericCat);
+
+        //Try overloaded methods with generically typed variables.
+        dex.testAnimal(genericDog);
+        dex.testAnimal(genericCat);
+    }
+
+    //Three overloaded methods.
+    public void testAnimal(Animal animal) {
+        System.out.println("Executing testAnimal with animal type");
+        animal.printAnimal();
+    }
+
+    public void testAnimal(Cat cat) {
+        System.out.println("Executing testAnimal with cat type");
+        cat.printAnimal();
+    }
+
+    public void testAnimal(Dog dog) {
+        System.out.println("Executing testAnimal with dog type");
+        dog.printAnimal();
+    }
+
+    //Specifically Typed Method calls
+    public void testDog(Dog dog) {
+        dog.printDog();
+    }
+
+    public void testCat(Cat cat) {
+        cat.printCat();
+    }
+
+}
