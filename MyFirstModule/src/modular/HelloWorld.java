1a2fedbcf9f736f7d147f488fe7ca73a95db1b37
Raiz <mohamed_raiz@hotmail.com>
2021-11-06 16:02:38 +0800

    more updates

--- MyFirstModule/src/modular/HelloWorld.java
+++ MyFirstModule/src/modular/HelloWorld.java
@@ -6,8 +6,11 @@ Section 12: Understanding Modules
 Topic: Describe how a modular project is compiled and run.
 */
 
+import mod2.GoodbyeWorld;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello new modular world test");
-    }
+        GoodbyeWorld.main(args);
+}
 }
